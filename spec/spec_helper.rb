6296b9f23f94527b183e52103bca95f4940a58cc
sanjeev mishra <sanjusoftware@gmail.com>
2017-09-07 12:59:24 +0700

    refactored spec file to remove unnecessary double quotes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "gitlab/devops"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'gitlab/devops'
 
 require 'rspec'
