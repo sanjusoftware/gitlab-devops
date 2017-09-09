9c793035cf498e94614b165a12fae22a27fbc0c8
sanjeev mishra <sanjusoftware@gmail.com>
2017-09-09 17:25:46 +0700

    added more tests and completed project settings

--- lib/gitlab/devops.rb
+++ lib/gitlab/devops.rb
@@ -12,12 +12,26 @@ module Gitlab
         end
 
         group_configs = options['groups']
-        group_configs.each do |group_config|
-          raise Error::Parsing, 'Group name must be provided' unless group_config['name']
-          groups = Gitlab.group_search(group_config['name'])
-          raise Error::Error, "group not found for #{group_config['name']}" if groups.empty?
-          groups.each do |group|
-            apply_group_settings group, group_config
+        if group_configs
+          group_configs.each do |group_config|
+            raise Error::Parsing, 'Group name must be provided' unless group_config['name']
+            groups = Gitlab.group_search(group_config['name'])
+            raise Error::Error, "group not found for #{group_config['name']}" if groups.empty?
+            groups.each do |group|
+              apply_group_settings group, group_config
+            end
+          end
+        end
+
+        project_configs = options['projects']
+        if project_configs
+          project_configs.each do |project_config|
+            raise Error::Parsing, 'Project name must be provided' unless project_config['name']
+            projects = Gitlab.project_search(project_config['name'])
+            raise Error::Error, "project not found for #{project_config['name']}" if projects.empty?
+            projects.each do |project|
+              apply_project_settings project, project_config['settings']
+            end
           end
         end
 
@@ -25,9 +39,46 @@ module Gitlab
 
       def self.apply_group_settings(group, group_config)
         projects = Gitlab.group_projects(group.id)
-        projects.each do |project|
-          apply_project_settings(project, group_config['settings'])
+
+        project_specific_configs = group_config['projects']
+
+        project_specific_configs.each do |project_config|
+          proj_name = project_config['name']
+          project = projects.find {|p| p.name == proj_name}
+          raise Error::Error, "Project with name #{proj_name} not found under group under #{group_config['name']}" unless project
         end
+
+        projects.each do |project|
+          proj_config = project_specific_configs.find {|proj| proj['name'] == project.name}
+          project_config = proj_config && proj_config['settings'] ? merge_settings(proj_config['settings'], group_config['settings']) : group_config['settings']
+
+          apply_project_settings(project, project_config)
+        end
+
+      end
+
+      def self.merge_settings(proj_settings, grp_settings)
+        return unless proj_settings
+        grp_settings['variables'] = merge_variables(proj_settings['variables'], grp_settings['variables']) if proj_settings['variables']
+        grp_settings
+      end
+
+      def self.merge_variables(proj_vars, grp_vars)
+        p grp_vars.inspect
+        grp_var_names = grp_vars.collect {|hash| hash['key']}
+
+        proj_vars.each do |hash|
+          if grp_var_names.include?(hash['key'])
+            update_grp_var(grp_vars, hash['key'], hash['value'])
+          else
+            grp_vars << hash
+          end
+        end
+        grp_vars
+      end
+
+      def self.update_grp_var(grp_vars, key_to_replace, new_value)
+        grp_vars.inject {|hash| hash['value'] = new_value if hash['key'] == key_to_replace}
       end
 
       def self.apply_project_settings(project, project_config)
@@ -37,7 +88,7 @@ module Gitlab
             when 'variables'
               update_project_variables(project, value) if project.name == 'global-project-settings'
             else
-              raise "Unsupported setting in config file: #{setting}. Please make sure all setting names mentioned in gitlab.config.yml file are in the defined list"
+              raise Error::Error, "Unsupported setting '#{setting}'. See supported in examples/gitlab.config.yml file"
           end
         end
       end
--- spec/gitlab/devops_spec.rb
+++ spec/gitlab/devops_spec.rb
@@ -11,14 +11,46 @@ describe Gitlab::Devops::Config do
       expect(Gitlab::Devops::VERSION).not_to be_nil
     end
 
-    it 'should throw_error if group name not provided' do
-      expect{with_config('../fixtures/config_no_grp_name.yml')}.to raise_error Gitlab::Error::Parsing
+    it 'should apply group settings' do
+      expect(with_config('../fixtures/config.yml')).to be_truthy
     end
 
-    it 'should throw_error if group does not exists' do
-      gitlab = double("Gitlab")
-      allow(gitlab).to receive(:group_search).and_return([])
-      expect{with_config('../fixtures/config_no_grp_name.yml')}.to raise_error Gitlab::Error::Error
+    it 'should throw error if config has unsupported setting' do
+      projects = [Gitlab::ObjectifiedHash.new({name: 'valid_project'})]
+      allow(Gitlab).to receive(:project_search).with('valid_name').and_return(projects)
+      expect {with_config('../fixtures/config_not_supported_settings.yml')}.to raise_error Gitlab::Error::Error,
+                                                                                           "Unsupported setting 'unsupported'. See supported in examples/gitlab.config.yml file"
     end
+
+    it 'should throw error if group name not provided' do
+      expect {with_config('../fixtures/config_no_grp_name.yml')}.to raise_error Gitlab::Error::Parsing,
+                                                                                'Group name must be provided'
+    end
+
+    it 'should throw error if group with given name not found' do
+      allow(Gitlab).to receive(:group_search).with('wrong_grp_name').and_return([])
+      expect {with_config('../fixtures/config_wrong_grp_name.yml')}.to raise_error Gitlab::Error::Error,
+                                                                                   'group not found for wrong_grp_name'
+    end
+
+    it 'should throw error if project under a group with given name not found' do
+      grps = [Gitlab::ObjectifiedHash.new({id: 1, name: 'group_name'})]
+      allow(Gitlab).to receive(:group_search).with('group_name').and_return(grps)
+      allow(Gitlab).to receive(:group_projects).with(1).and_return([])
+      expect {with_config('../fixtures/config_wrong_proj_name1.yml')}.to raise_error Gitlab::Error::Error,
+                                                                                     'Project with name wrong_proj_name not found under group under group_name'
+    end
+
+    it 'should throw_error if a project name not provided' do
+      expect {with_config('../fixtures/config_no_proj_name.yml')}.to raise_error Gitlab::Error::Parsing,
+                                                                                 'Project name must be provided'
+    end
+
+    it 'should throw_error if a project with given name not found' do
+      allow(Gitlab).to receive(:project_search).with('wrong_proj_name').and_return([])
+      expect {with_config('../fixtures/config_wrong_proj_name.yml')}.to raise_error Gitlab::Error::Error,
+                                                                                    'project not found for wrong_proj_name'
+    end
+
   end
 end
